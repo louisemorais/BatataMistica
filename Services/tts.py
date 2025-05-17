8cf4129226d923281ad22ab4402ff13f585a6cf8
onmikronDev <onmikron1@gmail.com>
2025-05-17 14:12:00 -0300

    tete é se

---
+++ Services/tts.py
@@ -0,0 +1,23 @@
+import pyttsx3
+
+
+voice = pyttsx3.init()
+
+voice.setProperty('language', 'pt-br')
+
+def speak(text):
+    # Define a voz
+    voices = voice.getProperty('voices')
+    for v in voices:
+        if 'brazil' in v.languages:
+            voice.setProperty('voice', v.id)
+            break
+
+    # Define a taxa de fala
+    voice.setProperty('rate', 200)
+
+    # Fala o texto
+    voice.say(text)
+    voice.runAndWait()
+
+
